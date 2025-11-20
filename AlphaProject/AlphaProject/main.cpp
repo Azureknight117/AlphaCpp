7c473a82845f3e9309c20fcbd45f398f8520571f
Antoniox <antonioeh7@gmail.com>
2025-11-20 12:23:26 -0600

    Pointers yay

--- AlphaProject/AlphaProject/main.cpp
+++ AlphaProject/AlphaProject/main.cpp
@@ -1,17 +1,57 @@
 #include <iostream>
 #include "StringInput.h"
 
+inline int Add(int x, int y) {
+	return x + y;
+}
+
 
 int main() {
 	std::cout << "Hello World!!\n";
 
-	int l = 77;
-	int i { 66 };
-	std::cout << i << " " << l << "\n";
+	std::cout << "\nPointer stuff\n";
+	int x{};
+	int *ptr = &x;
+	int *nPtr = nullptr;
+	std::cout << ptr << "\n";
+	std::cout << *ptr << "\n";
+	std::cout << nPtr << "\n";
+	nPtr = ptr;
+	std::cout << nPtr << "\n";
 
-	std::string greet = GetGreeting();
+	std::cout << "\nReference stuff\n";
 
-	std::cout << "You said: " << greet;
+	int o = 15;
+	int &oR = o;
+
+	std::cout << o << " and its ref: " << oR << "\n";
+	std::cout << "Adding 7 to them" << "\n";
+	oR += 7;
+	std::cout << o << " and its ref: " << oR << "\n";
+
+	std::cout << Add(4000, 58433) << "\n";
+
+	std::cout << "\Malloc stuff\n";
+
+	//int *p = (int*)malloc(sizeof(int));
+	int* p = (int*)calloc(1, sizeof(int));
+	if (p == NULL)
+	{
+		printf("failed to allocate memory \n");
+		return -1;
+	}
+	*p = 5;
+	std::cout << *p <<  "\n";
+	free(p);
+	p = NULL; //Do this pls
+
+	std::cout << "\New stuff (the operator) \n"; //Just use New not Malloc
+	int* k = new int;
+	*k = 99;
+	std::cout << *k << "\n";
+	delete(k);
+	k = NULL;
+
 	return 0;
 }
 
