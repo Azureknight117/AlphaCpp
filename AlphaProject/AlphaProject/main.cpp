4de8025df70c11479f2e8a54e17f09e8a6c31f3f
Antoniox <antonioeh7@gmail.com>
2025-11-25 10:05:10 -0600

    More templates

--- AlphaProject/AlphaProject/main.cpp
+++ AlphaProject/AlphaProject/main.cpp
@@ -2,19 +2,18 @@
 #include "StringInput.h"
 #include "Integer.h"
 #include "Testing.h"
+#include <string>
 
-
-template <typename T> T Max(T x, T y)
+template <typename T>
+T Max(T x, T y)
 {
-	return (x > y) ? x : y;
+    return (x > y) ? x : y;
 }
 
-
-
 int main() {
-	
-
-	std::cout << "x: " << Max(6.4, 10.5) << std::endl;
-	return 0;
+    std::cout << Max(15, 7) << std::endl;
+    std::cout << Max(3.14, 6.28) << std::endl;
+    std::cout << Max(std::string("apple"), std::string("banana")) << std::endl;
+    return 0;
 }
 
